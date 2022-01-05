f31838628ab356ed4267812a50ff8cf931720aeb
Sharvari More <83008601+SharvariMore@users.noreply.github.com>
2022-01-06 01:20:25 +0530

    Added content for uploading file

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-<script>
-    This is a js file
-</script>
+const realFileBtn = document.getElementById("real-file");
+const customBtn = document.getElementById("custom-button");
+const customTxt = document.getElementById("custom-text");
+
+customBtn.addEventListener("click", function(){
+    realFileBtn.click();
+});
+
+realFileBtn.addEventListener("change", function() {
+    if(realFileBtn.value)
+    {
+        customTxt.innerHTML = realFileBtn.value.match(/[\/\\]([\w\d\s\.\-\(\)]+)$/)[1];
+        alert("File uploaded successfully!")
+    }
+    else
+    {
+        customTxt.innerHTML = "No file chosen, yet";
+    }
+});
